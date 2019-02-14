7cf7975dfeebab9ef8b090744f0bf687ed1e0cfd
Zachary McGrath <zacharymcgrath@Zacharys-MacBook-Pro.local>
2019-02-14 11:18:36 -0600

    added gatherig of number of discrete values

--- main.py
+++ main.py
@@ -4,19 +4,33 @@ import kmeans as km
 import matplotlib.pyplot as plt
 
 def main(args):
+    discrete_column_number = 0
+    
     # make sure data set passed in
     if not args[1] or 'csv' not in args[1]:
         print('Error: no data csv passed in. Maker sure data is in csv format')
     input_file = args[1]
     data = np.genfromtxt(input_file, delimiter=',', skip_header=1, dtype=float)
 
+    # determine the number of discrete values there are for the number of clusters
+    if args[2]: 
+        discrete_column_number = int(args[2])
+
+    discrete_col = np.genfromtxt(input_file, delimiter=',', skip_header=1, usecols=discrete_column_number, dtype=str)
+    discrete_vals = {}
+    for a in discrete_col:
+        if a not in discrete_vals:
+            discrete_vals[a] = 1
+
+    num_discrete = len(discrete_vals)
+
     # TODO: parameterize later
     data = np.delete(data, np.s_[0:2], 1)
     data = np.delete(data, np.s_[1:5], 1)
     data = np.delete(data, np.s_[2:len(data)], 1)
 
-    # cluster number for k_means
-    clusters = 2
+    # cluster number AKA number of discrete values
+    clusters = num_discrete
 
     # check for model type
     if not args[2] or not 'em' in args[2]:
@@ -24,8 +38,8 @@ def main(args):
             clusters = int(args[2])
         centers, index = km.k_means(data, clusters)
         plt.scatter(data[:, 0], data[:, 1], c=index,
-            s=50, cmap='viridis');
-        plt.show();
+            s=50, cmap='viridis')
+        plt.show()
 
 
 if __name__ == '__main__':
