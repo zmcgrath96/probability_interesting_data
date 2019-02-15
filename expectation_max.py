Fixed the sigma computation, but results still look off

expectation_max.py
@@ -2,30 +2,23 @@ import numpy as np
 from scipy.stats import multivariate_normal
 import scipy.stats as ss
 
-def em(dataset, clusters, tol=0.01, max_iter=10000000):
+def em(dataset, clusters, tol=0.01, max_iter=1000):
     if dataset is None:
         print('No data')
         return
     if clusters is None:
         print('No clusters')
         return
-
     # weight for each point to each cluster
     pis = [1/float(clusters) for _ in range(clusters)]
     pis = np.array(pis)
 
     # parameter means
-    mus = [1 for _ in range(clusters)]
-    mus = np.array(mus)
     mus = np.random.random(clusters)
 
     # covariance
-    sigmas = [1 for _ in range(clusters)]
-    sigmas = np.array(sigmas)
     sigmas = np.random.random(clusters)
 
-    print(mus)
-    print(sigmas)
     # likelihood variables
     ll_old = 0.0
     ll_new = 1.0
@@ -43,13 +36,17 @@ def em(dataset, clusters, tol=0.01, max_iter=10000000):
             for j in range(clusters):
                 r_ic[i, j] = pis[j] * multivariate_normal(mus[j], sigmas[j]).pdf(dataset[i])
                 sum += r_ic[i, j]
-            r_ic[i] /= sum
-            
+            if sum != 0:
+                r_ic[i] /= sum
+
         # M Step
         # compute m_c
         m_c = np.zeros(clusters)
         for i in range(len(m_c)):
-            m_c[i] = np.sum(r_ic[:,i])
+            sum = 0
+            for j in range(len(dataset)):
+                sum += r_ic[j, i]
+            m_c[i] = sum
 
         # compute news pis
         for i in range(clusters):
@@ -57,27 +54,34 @@ def em(dataset, clusters, tol=0.01, max_iter=10000000):
 
         #compute new mus
         for i in range(clusters):
-            mus[i] = 1/m_c[i] * np.sum(r_ic[:,i]) * np.sum(dataset)
+            sum = 0
+            for j in range(len(dataset)):
+                sum += (r_ic[j, i] * dataset[i])
+            mus[i] = (1 / mus[i]) * sum
 
         #computes new sigmas
         for i in range(clusters):
-            temp = np.zeros(clusters)
-            for j in range(len(dataset)):
-                xMinusMu = dataset[j] - mus
-                #print("x-mu: {}".format(xMinusMu))
-                #print("x-mu dot: {}".format(np.dot((xMinusMu).T, (xMinusMu))))
-                temp[i] += r_ic[j, i] * np.dot((xMinusMu).T, (xMinusMu))
-            sigmas[i] = (1/m_c[i]) * temp[i]
+            xMinusMu = dataset - mus[i]
+            r_ic_temp = np.array(r_ic[:,i]).reshape(len(dataset), 1)
+            xMinusMuTransposed = xMinusMu.T
+            r_ic_temp *= np.dot(xMinusMuTransposed, xMinusMu)
+            sigmas[i] = (1/m_c[i]) * np.sum(r_ic_temp)
+
+
         #print("sigmas: {}".format(sigmas))
+
         # compute new likelyhood
         ll_new = 0.0
         for i in range(clusters):
             z = 0
             for j in range(len(dataset)):
                 z+= pis[i] * multivariate_normal(mus[i], sigmas[i]).pdf(dataset[j])
-            ll_new += np.log(z)
+            #ll_new += np.log(z)
         if abs(ll_new - ll_old) < tol:
             break
         ll_old = ll_new
         iterations += 1
-    return mus, sigmas
+    pdfs = np.empty((clusters, len(dataset)))
+    for i in range(clusters):
+        pdfs[i] = multivariate_normal.pdf(dataset, mean=mus[i], cov=sigmas[i])
+    return pdfs

main.py
@@ -3,6 +3,7 @@ import sys
 import kmeans as km
 import expectation_max as em
 import matplotlib.pyplot as plt
+import scipy.stats as ss
 
 def main(args):
     discrete_column_number = 0
@@ -47,10 +48,10 @@ def main(args):
         plt.show()
 
     else:
-        mus, sigmas = em.em(data[0], clusters)
-        print("mus: {}".format(mus))
-        print("sigmas: {}".format(sigmas))
-        #plt.plot(data[:, 0], pdfs[0])
+        pdfs = em.em(data[:, 0], clusters)
+        for i in range(len(pdfs)):
+            plt.plot(data[:,0], pdfs[i])
+        plt.show()
 
 
 if __name__ == '__main__':